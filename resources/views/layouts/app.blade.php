b528aca500f2b130ec3e0025cef462628db8bffd
Lucas Lopes <lopslucas12@gmail.com>
2026-02-24 17:26:13 -0300

    feat: adiciona suporte ao tema USP e configura menu administrativo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> -->
-</head>
-<body>
-    @section('content') @show()
-    
-</body>
-</html>
+@extends('laravel-usp-theme::master')
+
+@section('javascripts_bottom')
+    @parent
+    <script type="text/javascript" src="{ { asset('js/livro.js') } }"></script>
+@endsection
