b6f6b293cf508f7047a3699cebd9eff896a66065
Lucas Lopes <lopslucas12@gmail.com>
2026-02-24 15:32:36 -0300

    Deletando arquivos aula 1

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LivroController;
+// use App\Http\Controllers\LivroController;
 
-Route::get('/livros', [LivroController::class, 'index']);
+// Route::get('/livros', [LivroController::class, 'index']);
 
-Route::get('/livros/{isbn}', [LivroController::class, 'show']);
+// Route::get('/livros/{isbn}', [LivroController::class, 'show']);
